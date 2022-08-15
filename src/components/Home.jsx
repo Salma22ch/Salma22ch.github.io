9978eb396591002457412b0152ecd6b818009457
SALMA CHANA <chsalma22@gmail.com>
2022-08-15 12:45:25 +0200

    second

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Tilt from 'react-vanilla-tilt'
+import phone from '../assets/phone.png'
+import github from '../assets/github.png'
+import mail from '../assets/mail.png'
+import linkedin from '../assets/linkedin.png'
 import '../index.css'
 
 const Home = () => {
@@ -17,7 +21,13 @@ const Home = () => {
 
             <div className="info">
                     <h2> Hi, I'm  Salma CHANA 👋</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam aperiam odit temporibus nesciunt quibusdam. Voluptate perspiciatis consequatur quam debitis fuga mollitia quos voluptates vitae, aut nihil velit, consequuntur cupiditate neque?</p>
+                    <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam aperiam odit temporibus nesciunt quibusdam. Voluptate perspiciatis consequatur quam debitis fuga mollitia quos voluptates vitae, aut nihil velit, consequuntur cupiditate neque?</p>
+                    <div className="links">
+                        <a href="mailto:salmachana219@gmail.com"><img src={mail}/></a>
+                        <a href="https://www.linkedin.com/in/chana-salma/"><img src={linkedin}  /></a>
+                        <a href="https://github.com/Salma22ch"><img src={github}/></a>
+                        <a href="tel:+33752114227"><img src={phone} /></a>
+                    </div>
             </div>
              
             
