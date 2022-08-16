2b4a11c32ef822015f6236ae9ddc52c570955646
SALMA CHANA <chsalma22@gmail.com>
2022-08-16 13:39:28 +0200

    third

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Footer, NavBar, Aboutme, Home } from './components';
+import { Footer, NavBar, Aboutme, Home, Skills, Projects } from './components';
 import Title from 'react-vanilla-tilt'
 import './index.css';
 
@@ -17,6 +17,12 @@ const App = () => (
         </Route>
         <Route exact path="/aboutme">
           <Aboutme />
+        </Route>
+        <Route exact path="/skills">
+          <Skills />
+        </Route>
+        <Route exact path="/projects">
+          <Projects />
         </Route>
       </Switch>
     </BrowserRouter>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import Tilt from 'react-vanilla-tilt'
 
 const Skills = () => {
   return (
-    <div>
-      Skills
+    <div className="main-skills-container">
+      <div className="skills-header">
+        {/* 3d model */}
+        <h2> 💻 </h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas earum cumque blanditiis hic, exercitationem sunt vero dolor doloribus odio repudiandae obcaecati, quam sint, deleniti laboriosam suscipit non libero deserunt rerum!</p>
+        <h2> ......... </h2>
+      </div>
+      <div className="skills-card">
+        {/* <Tilt options={{ scale: 2 }} id="tilt-single-card"> */}
+          <div className="single-card">
+            <h4>UI interfaces
+            </h4>
+          </div>
+        {/* </Tilt> */}
+
+        <div className="single-card">
+          <h4>3D developement
+          </h4>
+        </div>
+        <div className="single-card">
+          <h4>Data visualisation
+          </h4>
+        </div>
+        <div className="single-card">
+          <h4>Computer vision
+          </h4>
+        </div>
+      </div>
+
+
     </div>
   )
 }
