18813e35a7052a588498ff6bf8fe3b9143a31ffc
SALMA CHANA <chsalma22@gmail.com>
2022-08-27 16:51:36 +0200

    keyword triggers useeffect

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,16 +5,16 @@ const Projects = () => {
 
   //initial projects list 
   var data=require("../projects.json")
-  var keyword='';
-  const [projects, setProjects] = useState(data);
+  const [keyword, setkeyword]=useState('');
+  const [projects, setProjects] = useState(data.projects);
 
 
   useEffect(()=>{
     //perfect filer works
-    const filteredprojects=keyword===''?data:Object.values(projects.projects).filter((p)=>p.tag.includes(keyword))
+    const filteredprojects=keyword===''?data:data.projects.filter((p)=>p.tag.includes(keyword))
     setProjects(filteredprojects);
-    console.log(filteredprojects,"salma");
-  })
+    console.log(filteredprojects,keyword);
+  }, [keyword]) // keyword when changed calls useEffect
 
   console.log(typeof(projects))
   // e.target.outerText
@@ -24,7 +24,7 @@ const Projects = () => {
       <div className="projects-header">
           <h2>Proj🚀cts</h2>
           <div className="key-words">
-            <span style={{backgroundColor:"blue"}} onClick={e => keyword= e.target.outerText}>#web-dev</span>
+            <span style={{backgroundColor:"blue"}} onClick={e => setkeyword(e.target.outerText)}>#web-dev</span>
             <span style={{backgroundColor:"peru"}}>#mobile-dev</span>
             <span style={{backgroundColor:"purple"}}>#3D/VR -dev</span>
             <span style={{backgroundColor:"crimson"}}>#Computer vision</span>
