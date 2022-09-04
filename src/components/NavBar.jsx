8ac871d8ca69c3b9b722886eb9d9087629082d71
SALMA CHANA <chsalma22@gmail.com>
2022-09-04 17:39:41 +0200

    setup gh-pages

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
-import { makeStyles } from "@material-ui/core/styles"
 import '../index.css'
 import { Link } from 'react-scroll';
+import useMediaQuery from "./useMediaQuery";
 
+import {FiHome,FiUser,FiAward,FiBox,FiFileText} from 'react-icons/fi';
 
 
 
 const NavBar = () => {
 
-
+  const isMobile = useMediaQuery('(min-width: 800px)');
 
   return (
-    <nav className='nav-bar'>
-      <Link activeClass="active" to="home" spy={true} smooth={true}>Home</Link>
-      <Link activeClass="active" to="about" spy={true} smooth={true}>Aboutme</Link>
-      <Link activeClass="active" to="skills" spy={true} smooth={true}>Skills</Link>
-      <Link activeClass="active" to="projects" spy={true} smooth={true}>Projects</Link>
-      <a href="https://drive.google.com/file/d/1THvQ7xDDcuoLGN1jXq10LC5z1LLPu4Gx/view?usp=sharing" target="_blank">Resume</a>
-      {/* <Link activeClass="active" to="Resume" spy={true} smooth={true}>Resume</Link> */}
-     {/* resume */}
-    </nav>
+    <div>
+      {isMobile ? 
+        <nav className='nav-bar'>
+          <Link activeClass="active" to="home" spy={true} smooth={true}>Home</Link>
+          <Link activeClass="active" to="about" spy={true} smooth={true}>Aboutme</Link>
+          <Link activeClass="active" to="skills" spy={true} smooth={true}>Skills</Link>
+          <Link activeClass="active" to="projects" spy={true} smooth={true}>Projects</Link>
+          <a href="https://drive.google.com/file/d/1THvQ7xDDcuoLGN1jXq10LC5z1LLPu4Gx/view?usp=sharing" target="_blank">Resume</a>
+        </nav> : 
+        <nav className='nav-bar'>
+          <Link activeClass="active" to="home" spy={true} smooth={true}><FiHome/></Link>
+          <Link activeClass="active" to="about" spy={true} smooth={true}><FiUser/></Link>
+          <Link activeClass="active" to="skills" spy={true} smooth={true}><FiAward/></Link>
+          <Link activeClass="active" to="projects" spy={true} smooth={true}><FiBox/></Link>
+          <a href="https://drive.google.com/file/d/1THvQ7xDDcuoLGN1jXq10LC5z1LLPu4Gx/view?usp=sharing" target="_blank"><FiFileText/></a>
+        </nav>}
+
+    </div>
+
   )
 }
 
