0eb1c6530fe8e6e39f0d99b19a6ff5105a1d64c8
Salma Chana <chsalma22@gmail.com>
2025-01-11 11:43:13 +0100

    remove resume

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,14 +19,12 @@ const NavBar = () => {
           <Link activeClass="active" to="about" spy={true} smooth={true}><CustomIcon Icon={FiUser} title={" Aboutme"}/> </Link>
           <Link activeClass="active" to="skills" spy={true} smooth={true}><CustomIcon Icon={FiAward} title={" Skills"}/> </Link>
           <Link activeClass="active" to="projects" spy={true} smooth={true}><CustomIcon Icon={FiBox} title={" Projects"}/> </Link>
-          <a href="https://drive.google.com/file/d/1THvQ7xDDcuoLGN1jXq10LC5z1LLPu4Gx/view?usp=sharing" target="_blank"><CustomIcon Icon={FiFileText} title={" Resume"}/> </a>
         </nav> : 
         <nav className='nav-bar'>
           <Link activeClass="active" to="home" spy={true} smooth={true}><CustomIcon Icon={FiHome}/></Link>
           <Link activeClass="active" to="about" spy={true} smooth={true}><CustomIcon Icon={FiUser}/></Link>
           <Link activeClass="active" to="skills" spy={true} smooth={true}><CustomIcon Icon={FiAward}/></Link>
           <Link activeClass="active" to="projects" spy={true} smooth={true}><CustomIcon Icon={FiBox}/></Link>
-          <a href="https://drive.google.com/file/d/1THvQ7xDDcuoLGN1jXq10LC5z1LLPu4Gx/view?usp=sharing" target="_blank"><FiFileText/></a>
         </nav>}
 
     </div>
